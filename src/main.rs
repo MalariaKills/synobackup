e2cfbb288a54f2d5f618b31037b835d6ecd3718c
marcus <mo.sunday@proton.me>
2025-06-21 20:51:40 -0500

    Added logic from the standard fs lib that will copy a full directory to a new location

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use std::fs;
+use std::fs::read_dir;
 use std::path::PathBuf;
 
 fn main() -> std::io::Result<()> {
-    let source = PathBuf::from("/home/marcus/Documents/rust_test/test_file.txt");
-    let destination = PathBuf::from("/home/marcus/Desktop/test_file.txt");
+    let source = PathBuf::from("/home/marcus/Documents/rust_test/2025-06-19");
+    let destination = PathBuf::from("/home/marcus/Desktop");
 
-    fs::copy(&source, &destination)?;
-    println!("File copied successfully from {:?} to {:?}", source, destination);
+    for entry in read_dir(&source)? {
+        let entry = entry?;
+        let source_path = entry.path();
+        let file_name = entry.file_name();
+        let destination_path = destination.join(file_name);
+
+        fs::copy(&source_path, &destination_path)?;
+
+        println!(
+            "File copied successfully from {:?} to {:?}",
+            source, destination
+        );
+    }
 
     Ok(())
 }
